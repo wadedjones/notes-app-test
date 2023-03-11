d8ea09bf3fb0ece889e741eb8b6c97306dfac13e
Wade Jones <wadedjones@gmail.com>
2023-03-10 18:13:30 -0800

    connected POST and note/id to backend

--- index.js
+++ index.js
@@ -9,24 +9,6 @@ app.use(express.json())
 app.use(express.static('build'))
 app.use(cors())
 
-let notes = [
-    {
-        id: 1,
-        content: 'HTML is easy',
-        important: true
-    },
-    {
-        id: 2,
-        content: 'Browser can execute only JavaScript',
-        important: false
-    },
-    {
-        id: 3,
-        content: 'GET and POST are the most important methods of HTTP protocol',
-        important: true
-    }
-]
-
 app.get('/api/notes', (request, response) => {
     Note.find({}).then(notes => {
         response.json(notes)
@@ -34,18 +16,9 @@ app.get('/api/notes', (request, response) => {
 })
 
 app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    console.log(id)
-    const note = notes.find(note => note.id === id)
-    /*const note = notes.find(note => {
-        console.log(note.id, typeof note.id, id, typeof id, note.id === id)
-        return note.id === id
-    })*/
-    if (note) {
+    Note.findById(request.params.id).then(note => {
         response.json(note)
-    } else {
-        response.status(404).end()
-    }
+    })
 })
 
 app.delete('/api/notes/:id', (request, response) => {
@@ -53,13 +26,15 @@ app.delete('/api/notes/:id', (request, response) => {
     notes = notes.filter(note => note.id !== id)
     response.status(204).end()
 })
-
+/*
+// MongoDB generates IDs automatically
 const generateId = () => {
     const maxId = notes.length > 0
         ? Math.max(...notes.map(n => n.id))
         : 0
     return maxId + 1
 }
+*/
 
 app.post('/api/notes', (request, response) => {
     const body = request.body
@@ -69,13 +44,14 @@ app.post('/api/notes', (request, response) => {
         })
     }
 
-    const note = {
+    const note = new Note ({
         content: body.content,
         important: body.important || false,
-        id: generateId()
-    }
-    notes = notes.concat(note)
-    response.json(note)
+    })
+
+    note.save().then(savedNote => {
+        response.json(savedNote)
+    })
 })
 
 const PORT = process.env.PORT
